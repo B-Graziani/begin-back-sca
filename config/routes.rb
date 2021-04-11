9dbb8685ea4afa8730c7076915b2492f8d8b75cf
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-11 17:47:20 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
     resources :reviews, only: [:new, :create]
 end
-
+  resources :reviews, only:[:destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
